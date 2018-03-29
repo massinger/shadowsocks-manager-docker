eeff1d0fb5182bacb8d06e40430f251e868707f2
massinger <massinger@139.com>
2018-03-29 15:35:01 +0800

    修改入口文件

--- entry.sh
+++ entry.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 cd /etc/shadowsocks 
+
+
 cp -n ssmgr.yml.example ssmgr.yml 
-cp -n webui.yml.example webui.yml 
+cp -n webui.yml.example webui.yml && sed -i "s/\'username\'/\'$username\'/g" webui.yml && sed -i "s/\'password\'/\'$password\'/g" webui.yml && sed -i "s/\'smtp_server\'/\'$smtp_server'/g" webui.yml && sed -i "s/\'site_host'/\'$site_host'/g" webui.yml
+
 cp -n ss-server.json.example ss-server.json
+
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
